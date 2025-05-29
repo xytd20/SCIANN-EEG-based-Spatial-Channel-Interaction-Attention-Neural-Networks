ca52e8fc5c9cf2a73fa82ccc35adcf0df623748e
xytd20 <136790937+xytd20@users.noreply.github.com>
2025-05-29 17:35:33 +0800

    Update SCIANN.py

--- SCIANN.py
+++ SCIANN.py
@@ -1,3 +1,31 @@
+"""
+Copyright (C) 2025 Beihang University, China
+SPDX-License-Identifier: Apache-2.0
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+Author: Haiyang Long
+"""
+
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as sio
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+import json
+import csv
+import networkx as nx
+
 class AdjustableSpatialAttention(nn.Module):
     """可调整层数的改进空间注意力模块，用于捕获EEG电极间的复杂空间关系"""
     def __init__(self, num_channels, channel_layers=3, spatial_layers=2, reduction_ratio=4):
